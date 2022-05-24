ae98a1bf5cfbe2e46c53b6b4fdc2336fa5e5b0f2
Khan Hooi <khanhooi@qantas.com.au>
2022-05-24 21:39:06 +1000

    Add deployment environment

--- aws-cdk-deploy/bin/thatscooked.ts
+++ aws-cdk-deploy/bin/thatscooked.ts
@@ -4,14 +4,31 @@ import * as cdk from 'aws-cdk-lib';
 import { thatscookedStack } from '../lib/thatscooked-stack';
 
 const app = new cdk.App();
-new thatscookedStack(app, 'thats-cooked-stack', {
-  description: "A simple static website for thatscooked.net",
 
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-  stackName: "thats-cooked-master",
-  domain: "thatscooked.net",
-  staticAssetDirectory: './website-dist',
-
-  // Request certificate via console at Amazon Certificate Manager (ACM) (DNS validation is recommended):
-  certificateArn: "arn:aws:acm:us-east-1:945103061005:certificate/d6b67eca-ee19-4c3c-9464-acca382da17a",
-});
+console.log('DEPLOYMENT_ENVIRONMENT 👉', process.env.DEPLOYMENT_ENVIRONMENT);
+if ( process.env.DEPLOYMENT_ENVIRONMENT == 'master' )
+{
+  new thatscookedStack(app, 'thats-cooked-stack', {
+    description: "A simple static website for thatscooked.net",
+    env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+    stackName: "thats-cooked-master",
+    domain: "thatscooked.net",
+    staticAssetDirectory: './website-dist',
+  
+    // Request certificate via console at Amazon Certificate Manager (ACM) (DNS validation is recommended):
+    certificateArn: "arn:aws:acm:us-east-1:945103061005:certificate/d6b67eca-ee19-4c3c-9464-acca382da17a",
+  });
+}
+else
+{
+  new thatscookedStack(app, 'thats-cooked-dev-stack', {
+    description: "development stack for dev.thatscooked.net",
+    env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+    stackName: "thats-cooked-dev",
+    domain: "dev.thatscooked.net",
+    staticAssetDirectory: './website-dist',
+  
+    // Request certificate via console at Amazon Certificate Manager (ACM) (DNS validation is recommended):
+    certificateArn: "arn:aws:acm:us-east-1:945103061005:certificate/d6b67eca-ee19-4c3c-9464-acca382da17a",
+  });
+}
